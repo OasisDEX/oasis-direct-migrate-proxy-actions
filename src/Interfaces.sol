26df889beb470acb976806501adb2297f50cfd6e
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-02-01 13:31:27 -0300

    Update to float pragma

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.12;
+pragma solidity ^0.5.12;
 
 contract GemLike {
     function allowance(address, address) public returns (uint);
--- src/OasisDirectMigrateProxyActions.sol
+++ src/OasisDirectMigrateProxyActions.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.12;
+pragma solidity ^0.5.12;
 
 import { JoinLike, SaiTubLike, GemLike, OtcLike, ScdMcdMigrationLike } from "./Interfaces.sol";
 import "ds-math/math.sol";
